87354cb9381051b007c47adfef1a4bd9c6befcd8
Mariano Saura <1784443@alu.murciaeduca.com>
2016-11-23 14:09:24 +0100

    Moderación de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,4 +1,16 @@
 var models= require('../models/models.js');
+
+exports.load = function(req, res, next, commentId){
+	models.Comment.findById(commentId)
+		.then(function(comment){
+		if(comment){
+			req.comment = comment;
+			next();
+		}else{next(new Error('No existe commentId='+commentId))}
+	}
+	).catch(function(error){next(error)});
+}
+
 exports.new = function(req, res){
 	res.render('comments/new.ejs', {quizId: req.params.quizId, errors:[]});
 };
@@ -23,4 +35,12 @@ exports.create = function(req, res){
 		
 		}
 	).catch(function(error){next(error)});
+};
+
+exports.publish = function(req, res){
+	req.comment.publicado = true;
+
+	req.comment.save({fields: ["publicado"]})
+	.then(function(){res.redirect('/quizes/'+req.params.quizId);})
+	.catch(function(error){next(error)});
 };
